8508f2d9c8b80f5c6a21d06c10359387af5569e3
Fahmii19 <fhmialfi79@gmail.com>
2023-08-29 19:26:53 +0700

    first commit

---
+++ resources/views/partials/dashboard/sub-header.blade.php
@@ -0,0 +1,11 @@
+<div class="iq-navbar-header" style="height: 215px;">
+
+    <div class="iq-header-img" style="
+    padding-left: 5vh;
+    padding-right: 5vh;
+    margin-top: 14vh;
+">
+
+        <img style="object-fit: fill;" src="{{asset('images/dashboard/dashboard7.jpg')}}" alt="header" class="theme-color-default-img img-fluid w-100 h-100">
+    </div>
+</div>
