3bd51bd56e2f065b7cf688a0a44e2d858df79831
Fahmii19 <fhmialfi79@gmail.com>
2023-09-21 19:25:43 +0700

    perbaikan logic import

--- app/Imports/CalonPemilihImport.php
+++ app/Imports/CalonPemilihImport.php
@@ -20,14 +20,14 @@ class CalonPemilihImport implements ToModel, WithStartRow
         Log::info($row);
         try {
             return new CalonPemilih([
-                'nama_pemilih'   => isset($row[0]) ? strtoupper($row[0]) : 'N/A',
-                'jenis_kelamin'  => isset($row[1]) ? $row[1] : 'N/A',
-                'no_hp'          => isset($row[2]) ? $row[2] : 'N/A',
-                'rt'             => isset($row[3]) ? $row[3] : 'N/A',
-                'rw'             => isset($row[4]) ? $row[4] : 'N/A',
-                'tps'            => isset($row[5]) ? $row[5] : 'N/A',
-                'kelurahan'      => isset($row[6]) ? strtoupper($row[6]) : 'N/A',
-                'nik'            => 'N/A'  // Karena tidak ada di Excel Anda, saya set default value menjadi 'N/A'
+                'nik'            => isset($row[0]) ? $row[0] : 'N/A',
+                'nama_pemilih'   => isset($row[1]) ? strtoupper($row[1]) : 'N/A',
+                'jenis_kelamin'  => isset($row[2]) ? $row[2] : 'N/A',
+                'no_hp'          => isset($row[3]) ? $row[3] : 'N/A',
+                'rt'             => isset($row[4]) ? $row[4] : 'N/A',
+                'rw'             => isset($row[5]) ? $row[5] : 'N/A',
+                'tps'            => isset($row[6]) ? $row[6] : 'N/A',
+                'kelurahan'      => isset($row[7]) ? strtoupper($row[7]) : 'N/A',
             ]);
         } catch (\Exception $e) {
             // Hanya abaikan error dan lanjutkan
