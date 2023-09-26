0eaea401911204c36019a250c5d870684ee61110
Fahmii19 <fhmialfi79@gmail.com>
2023-09-26 23:17:33 +0700

    push

--- resources/views/dashboards/dashboard.blade.php
+++ resources/views/dashboards/dashboard.blade.php
@@ -2,119 +2,78 @@
     <div class="row">
         <div class="col-md-12 col-lg-12">
             <div class="row row-cols-1">
-                <div class="d-slider1 ">
-
+                <div class="d-slider1 overflow-hidden ">
                     <ul class="swiper-wrapper list-inline m-0 p-0 mb-2">
-
                         <li class="swiper-slide card card-slide" data-aos="fade-up" data-aos-delay="900">
                             <div class="card-body">
                                 <div class="progress-widget">
-                                    <div id="circle-progress-03" class="circle-progress-01 circle-progress circle-progress-primary text-center" data-min-value="0" data-max-value="100" data-value="100" data-type="percent">
-
-                                        <svg class="card-slie-arrow " width="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-
-                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M9.87651 15.2063C6.03251 15.2063 2.74951 15.7873 2.74951 18.1153C2.74951 20.4433 6.01251 21.0453 9.87651 21.0453C13.7215 21.0453 17.0035 20.4633 17.0035 18.1363C17.0035 15.8093 13.7415 15.2063 9.87651 15.2063Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M9.8766 11.886C12.3996 11.886 14.4446 9.841 14.4446 7.318C14.4446 4.795 12.3996 2.75 9.8766 2.75C7.3546 2.75 5.3096 4.795 5.3096 7.318C5.3006 9.832 7.3306 11.877 9.8456 11.886H9.8766Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                            <path d="M19.2036 8.66919V12.6792" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                            <path d="M21.2497 10.6741H17.1597" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                                    <div id="circle-progress-01" class="circle-progress-01 circle-progress circle-progress-primary text-center" data-min-value="0" data-max-value="100" data-value="90" data-type="percent">
+                                        <svg class="card-slie-arrow " width="24" height="24px" viewBox="0 0 24 24">
+                                            <path fill="currentColor" d="M5,17.59L15.59,7H9V5H19V15H17V8.41L6.41,19L5,17.59Z" />
                                         </svg>
-
-
                                     </div>
                                     <div class="progress-detail">
                                         <p class="mb-2">Total Koordinator</p>
                                         <h4 class="counter">{{ $jumlahKoordinator }}</h4>
 
                                     </div>
-                                </div>
-                            </div>
-                        </li>
 
-                        <li class="swiper-slide card card-slide aos-init aos-animate" data-aos="fade-up" data-aos-delay="1000" style="width: 276.25px; margin-right: 32px;" role="group" aria-label="4 / 7">
-                            <div class="card-body">
-                                <div class="progress-widget">
-                                    <div id="circle-progress-04" class="text-center circle-progress-01 circle-progress circle-progress-info" data-min-value="0" data-max-value="100" data-value="100" data-type="percent" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="100">
-
-                                        <svg class="card-slie-arrow icon-24" width="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-
-                                            <path d="M17.8877 10.8967C19.2827 10.7007 20.3567 9.50473 20.3597 8.05573C20.3597 6.62773 19.3187 5.44373 17.9537 5.21973" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                            <path d="M19.7285 14.2505C21.0795 14.4525 22.0225 14.9255 22.0225 15.9005C22.0225 16.5715 21.5785 17.0075 20.8605 17.2815" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M11.8867 14.6638C8.67273 14.6638 5.92773 15.1508 5.92773 17.0958C5.92773 19.0398 8.65573 19.5408 11.8867 19.5408C15.1007 19.5408 17.8447 19.0588 17.8447 17.1128C17.8447 15.1668 15.1177 14.6638 11.8867 14.6638Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M11.8869 11.888C13.9959 11.888 15.7059 10.179 15.7059 8.069C15.7059 5.96 13.9959 4.25 11.8869 4.25C9.7779 4.25 8.0679 5.96 8.0679 8.069C8.0599 10.171 9.7569 11.881 11.8589 11.888H11.8869Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                            <path d="M5.88509 10.8967C4.48909 10.7007 3.41609 9.50473 3.41309 8.05573C3.41309 6.62773 4.45409 5.44373 5.81909 5.21973" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                            <path d="M4.044 14.2505C2.693 14.4525 1.75 14.9255 1.75 15.9005C1.75 16.5715 2.194 17.0075 2.912 17.2815" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                        </svg>
-
-
-                                    </div>
-
-                                    <svg class="card-slie-arrow icon-24" width="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-
-                                        <path d="M17.8877 10.8967C19.2827 10.7007 20.3567 9.50473 20.3597 8.05573C20.3597 6.62773 19.3187 5.44373 17.9537 5.21973" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                        <path d="M19.7285 14.2505C21.0795 14.4525 22.0225 14.9255 22.0225 15.9005C22.0225 16.5715 21.5785 17.0075 20.8605 17.2815" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M11.8867 14.6638C8.67273 14.6638 5.92773 15.1508 5.92773 17.0958C5.92773 19.0398 8.65573 19.5408 11.8867 19.5408C15.1007 19.5408 17.8447 19.0588 17.8447 17.1128C17.8447 15.1668 15.1177 14.6638 11.8867 14.6638Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M11.8869 11.888C13.9959 11.888 15.7059 10.179 15.7059 8.069C15.7059 5.96 13.9959 4.25 11.8869 4.25C9.7779 4.25 8.0679 5.96 8.0679 8.069C8.0599 10.171 9.7569 11.881 11.8589 11.888H11.8869Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                        <path d="M5.88509 10.8967C4.48909 10.7007 3.41609 9.50473 3.41309 8.05573C3.41309 6.62773 4.45409 5.44373 5.81909 5.21973" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                        <path d="M4.044 14.2505C2.693 14.4525 1.75 14.9255 1.75 15.9005C1.75 16.5715 2.194 17.0075 2.912 17.2815" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                    </svg>
-
-
-                                    <div class="progress-detail">
-                                        <p class="mb-2">Calon Pemilih</p>
-                                        <h4 class="counter" style="visibility: visible;">{{ $jumlahCalonPemilih }}</h4>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-
-
-                        <li class="swiper-slide card card-slide" data-aos="fade-up" data-aos-delay="700">
-                            <div class="card-body">
-                                <div class="progress-widget">
-                                    <div id="circle-progress-01" class="circle-progress-01 circle-progress circle-progress-primary text-center" data-min-value="0" data-max-value="100" data-value="100" data-type="percent">
-
-                                        <svg class="card-slie-arrow " width="24" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-
-                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M13.1043 4.17701L14.9317 7.82776C15.1108 8.18616 15.4565 8.43467 15.8573 8.49218L19.9453 9.08062C20.9554 9.22644 21.3573 10.4505 20.6263 11.1519L17.6702 13.9924C17.3797 14.2718 17.2474 14.6733 17.3162 15.0676L18.0138 19.0778C18.1856 20.0698 17.1298 20.8267 16.227 20.3574L12.5732 18.4627C12.215 18.2768 11.786 18.2768 11.4268 18.4627L7.773 20.3574C6.87023 20.8267 5.81439 20.0698 5.98724 19.0778L6.68385 15.0676C6.75257 14.6733 6.62033 14.2718 6.32982 13.9924L3.37368 11.1519C2.64272 10.4505 3.04464 9.22644 4.05466 9.08062L8.14265 8.49218C8.54354 8.43467 8.89028 8.18616 9.06937 7.82776L10.8957 4.17701C11.3477 3.27433 12.6523 3.27433 13.1043 4.17701Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                        </svg>
-
-
-                                    </div>
-                                    <div class="progress-detail">
-                                        <p class="mb-2">Pemilih Tetap</p>
-                                        <h4 class="counter" style="visibility: visible;">
-                                            {{ $jumlahPemilih }}</h4>
-                                    </div>
                                 </div>
                             </div>
                         </li>
                         <li class="swiper-slide card card-slide" data-aos="fade-up" data-aos-delay="800">
                             <div class="card-body">
                                 <div class="progress-widget">
-                                    <div id="circle-progress-02" class="circle-progress-01 circle-progress circle-progress-info text-center" data-min-value="0" data-max-value="100" data-value="100" data-type="percent">
-
-
-                                        <svg class="card-slie-arrow " width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-
-                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M16.3345 2.75024H7.66549C4.64449 2.75024 2.75049 4.88924 2.75049 7.91624V16.0842C2.75049 19.1112 4.63549 21.2502 7.66549 21.2502H16.3335C19.3645 21.2502 21.2505 19.1112 21.2505 16.0842V7.91624C21.2505 4.88924 19.3645 2.75024 16.3345 2.75024Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                            <path d="M8.43994 12.0002L10.8139 14.3732L15.5599 9.6272" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                                    <div id="circle-progress-02" class="circle-progress-01 circle-progress circle-progress-info text-center" data-min-value="0" data-max-value="100" data-value="80" data-type="percent">
+                                        <svg class="card-slie-arrow " width="24" height="24" viewBox="0 0 24 24">
+                                            <path fill="currentColor" d="M19,6.41L17.59,5L7,15.59V9H5V19H15V17H8.41L19,6.41Z" />
                                         </svg>
+                                    </div>
+                                    <div class="progress-detail">
+                                        <p class="mb-2">Calon Pemilih</p>
+                                        <h4 class="counter" style="visibility: visible;">{{ $jumlahCalonPemilih }}</h4>
 
+                                    </div>
 
+                                </div>
+                            </div>
+                        </li>
+                        <li class="swiper-slide card card-slide" data-aos="fade-up" data-aos-delay="900">
+                            <div class="card-body">
+                                <div class="progress-widget">
+                                    <div id="circle-progress-03" class="circle-progress-01 circle-progress circle-progress-primary text-center" data-min-value="0" data-max-value="100" data-value="70" data-type="percent">
+                                        <svg class="card-slie-arrow " width="24" viewBox="0 0 24 24">
+                                            <path fill="currentColor" d="M19,6.41L17.59,5L7,15.59V9H5V19H15V17H8.41L19,6.41Z" />
+                                        </svg>
+                                    </div>
+                                    <div class="progress-detail">
+                                        <p class="mb-2">Pemilih Tetap</p>
+                                        <h4 class="counter" style="visibility: visible;">
+                                            {{ $jumlahPemilih }}</h4>
+                                    </div>
+
+                                </div>
+                        </li>
+                        <li class="swiper-slide card card-slide" data-aos="fade-up" data-aos-delay="1000">
+                            <div class="card-body">
+                                <div class="progress-widget">
+                                    <div id="circle-progress-04" class="circle-progress-01 circle-progress circle-progress-info text-center" data-min-value="0" data-max-value="100" data-value="60" data-type="percent">
+                                        <svg class="card-slie-arrow " width="24px" height="24px" viewBox="0 0 24 24">
+                                            <path fill="currentColor" d="M5,17.59L15.59,7H9V5H19V15H17V8.41L6.41,19L5,17.59Z" />
+                                        </svg>
                                     </div>
                                     <div class="progress-detail">
                                         <p class="mb-2">Akun Terdaftar</p>
                                         <h4 class="counter">{{ $jumlahAkunDcak }}</h4>
 
                                     </div>
+
                                 </div>
                             </div>
                         </li>
-
-
-
                     </ul>
+                    <div class="swiper-button swiper-button-next"></div>
+                    <div class="swiper-button swiper-button-prev"></div>
                 </div>
             </div>
         </div>
@@ -131,7 +90,7 @@
                             <div class="d-flex align-items-center align-self-center">
                                 <div class="d-flex align-items-center text-primary">
                                 </div>
-                                <div class="d-flex align-items-center ms-3 text-info">
+                                <div class="d-flex align-items-center ms-3 text-info" style="display:none;">
                                     <div class="dropdown">
                                         <a href="#" class="text-gray dropdown-toggle" id="dropdownMenuButton3" data-bs-toggle="dropdown" aria-expanded="false">
                                             Filter: <span class="jumlah_hari">7</span> hari
@@ -157,102 +116,4 @@
             </div>
         </div>
     </div>
-
-
-
-    {{-- <script>
-        const rawDataPemilih = @json($pemilih);
-
-        // Fungsi untuk mendapatkan 7 tanggal terakhir
-        function getLastSevenDays() {
-            const dates = [];
-            for (let i = 6; i >= 0; i--) {
-                const d = new Date();
-                d.setDate(d.getDate() - i);
-                dates.push(d.toISOString().split("T")[0]);
-            }
-            return dates;
-        }
-
-        const lastSevenDays = getLastSevenDays();
-
-        function aggregateDataByDate(data) {
-            let result = {
-                Tapos: {
-                    "Sukamaju Baru": Array(7).fill(0)
-                    , "Sukatani": Array(7).fill(0)
-                , }
-                , Cilodong: {
-                    "Kalimulya": Array(7).fill(0)
-                , }
-            };
-
-            data.forEach(pemilih => {
-                let date = pemilih.created_at.split(" ")[0];
-                let index = getLastSevenDays().indexOf(date);
-                if (index !== -1) {
-                    if (result[pemilih.kecamatan] && result[pemilih.kecamatan][pemilih.kelurahan]) {
-                        result[pemilih.kecamatan][pemilih.kelurahan][index]++;
-                    }
-                }
-            });
-
-            return result;
-        }
-
-        const kelurahanData = aggregateDataByDate(rawDataPemilih);
-
-        var ctx = document.getElementById("myChart").getContext("2d");
-        var myChart = new Chart(ctx, {
-            type: "line"
-            , data: {
-                labels: lastSevenDays
-                , datasets: [{
-                        label: "Tapos"
-                        , data: kelurahanData.Tapos["Sukamaju Baru"].map(
-                            (data, index) => data + kelurahanData.Tapos["Sukatani"][index]
-                        )
-                        , borderColor: "rgba(75, 192, 192, 1)"
-                        , fill: false
-                        , borderWidth: 2
-                    , }
-                    , {
-                        label: "Cilodong"
-                        , data: kelurahanData.Cilodong["Kalimulya"]
-                        , borderColor: "rgba(255, 99, 132, 1)"
-                        , fill: false
-                        , borderWidth: 2
-                    , }
-                , ]
-            , }
-            , options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    , }
-                , }
-                , plugins: {
-                    tooltip: {
-                        callbacks: {
-                            label: function(context) {
-                                const kecamatan = context.dataset.label;
-                                const dateIndex = context.dataIndex;
-                                const kelurahanLabels = Object.keys(kelurahanData[kecamatan]);
-                                const labels = [];
-
-                                kelurahanLabels.forEach((kel) => {
-                                    labels.push(kel + ": " + kelurahanData[kecamatan][kel][dateIndex]);
-                                });
-
-                                return labels;
-                            }
-                        , }
-                    , }
-                , }
-            , }
-        , });
-
-    </script> --}}
-
-
 </x-app-layout>
