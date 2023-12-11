71444d01255d2084b5480ce28a3f9e8b90e8a5f0
Frdzyi01 <riyanfirdzi@gmail.com>
2023-12-12 03:16:12 +0700

    pushhhhhh

--- resources/views/dcak/total-suara/index.blade.php
+++ resources/views/dcak/total-suara/index.blade.php
@@ -4,35 +4,34 @@
         #showModalKoordinator .modal-content {
             color: black;
         }
-
     </style>
 
 
 
     <div class="row">
-        @foreach($jumlahPemilihPerKelurahanPerkecamatan as $kecamatan => $kelurahans)
+        @foreach ($jumlahPemilihPerKelurahanPerkecamatan as $kecamatan => $kelurahans)
+            <div class="col-md-12 col-xl-6">
+                <div class="card aos-init aos-animate" data-aos="fade-up" data-aos-delay="900">
+                    <div class="flex-wrap card-header d-flex justify-content-between">
+                        <div class="header-title">
 
-        <div class="col-md-12 col-xl-6">
-            <div class="card aos-init aos-animate" data-aos="fade-up" data-aos-delay="900">
-                <div class="flex-wrap card-header d-flex justify-content-between">
-                    <div class="header-title">
-                        <h4 class="card-title">{{ $kecamatan }}</h4>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <div class="d-grid gap col-md-5 col-lg-5">
-                        @foreach($kelurahans as $kelurahan => $jumlah)
-                        <div class="flex-wrap d-flex justify-content-between">
-                            <div><span class="text-gray">{{ $kelurahan }}</span></div>
-                            <div>
-                                <h6>{{ $jumlah }}</h6>
-                            </div>
+                            <h4 class="card-title">Total Surat Suara {{ $kecamatan }}</h4>
                         </div>
-                        @endforeach
+                    </div>
+                    <div class="card-body">
+                        <div class="d-grid gap col-md-5 col-lg-5">
+                            @foreach ($kelurahans as $kelurahan => $jumlah)
+                                <div class="flex-wrap d-flex justify-content-between">
+                                    <div><span class="text-gray">{{ $kelurahan }}</span></div>
+                                    <div>
+                                        <h6>{{ $jumlah }}</h6>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         @endforeach
     </div>
 
